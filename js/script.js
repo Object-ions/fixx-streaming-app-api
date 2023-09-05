251fc7dd5ff1ae09cb601cf88ef772cf0e5ae0f1
object-ions <moshikoatia@gmail.com>
2023-09-05 11:29:52 -0700

    add switch cases to indicate which page we are currently on

---
+++ js/script.js
@@ -0,0 +1,27 @@
+ const global = {
+  currentPage: window.location.pathname
+};
+
+// Init app
+function init() {
+  switch (global.currentPage) {
+    case '/':
+    case '/index.html':
+      console.log('home');
+      break;
+    case '/shows.html':
+      console.log('shows');
+      break;
+    case '/movie-details.html':
+      console.log('movie-details');
+      break;
+    case '/tv-details.html':
+      console.log('tv-details');
+      break;
+    case '/search.html':
+      console.log('search');
+      break;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', init);
