25b903babf68990ce769adeb06ec8429c1136733
object-ions <moshikoatia@gmail.com>
2023-09-05 19:20:25 -0700

    add display backdrop on 'details' page

--- js/script.js
+++ js/script.js
@@ -91,6 +91,9 @@ async function displayMovieDetails() {
 
   const movie = await fetchAPIData(`movie/${movieId}`);
 
+  // Overlay for background image
+  displayBGImage('movie', movie.backdrop_path);
+
   const div = document.createElement('div');
   div.classList.add('card');
   div.innerHTML = `
@@ -142,6 +145,28 @@ async function displayMovieDetails() {
   `;
 
   document.querySelector('#movie-details').appendChild(div);
+}
+
+// Display backdrop on details pages
+function displayBGImage(type, backgroundPath) {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(http://image.tmdb.org/t/p/original/${backgroundPath})`;
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = 0;
+  overlayDiv.style.left = 0;
+  overlayDiv.style.opacity = 0.1 ;
+  overlayDiv.style.zIndex = -1;
+
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv);
+  } else {
+    document.querySelector('#show-details').appendChild(overlayDiv);
+  }
 }
 
 // Fetch data from TMDB API
